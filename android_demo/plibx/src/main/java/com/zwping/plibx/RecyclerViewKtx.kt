717227b33a2ef5cd1528cc9a0170b88da18ab045
zwping <1101558280@qq.com>
2021-07-09 18:25:37 +0800

    rvktx 增加datasStateCallback

--- android_demo/plibx/src/main/java/com/zwping/plibx/RecyclerViewKtx.kt
+++ android_demo/plibx/src/main/java/com/zwping/plibx/RecyclerViewKtx.kt
@@ -21,9 +21,19 @@ class RecyclerViewKtx
 abstract class BaseAdapter<E> : RecyclerView.Adapter<BaseVH<E, ViewBinding>>() {
 
     var datas = mutableListOf<E>()
+        private set(value) {
+            field = value
+            pag.curPage = 1
+            pag.curTotal = datas.size
+            datasStateCallback?.refreshState(true)
+            datasStateCallback?.noMoreData(pag.hasEnd())  // 默认协议每次返回数量为指定的PageSize(总数量%每页数量!=0 == 咩有更多数据)
+        }
+    var pag = Pagination()  // pageSize默认20
         private set
 
-    var diffCallback : DiffCallback<E>? = null  // 重新初始化 才可友善刷新
+    var datasStateCallback: DatasStataCallback? = null   // 控制RefreshLayout状态, util配置可兼容所有刷新控件
+    var diffCallback : DiffCallback<E>? = null           // 初始化 才可Diff渲染数据
+
     fun setData(data: MutableList<E>, detectMoves: Boolean = true) {
         if (null == diffCallback) {
             datas = data; notifyDataSetChanged()
@@ -32,12 +42,26 @@ abstract class BaseAdapter<E> : RecyclerView.Adapter<BaseVH<E, ViewBinding>>() {
         _od.clear(); _od.addAll(datas); _nd.clear(); _nd.addAll(data)
         DiffUtil.calculateDiff(_diffCallBack, detectMoves).dispatchUpdatesTo(this)
         datas = data
-
     }
-    fun addData(data: MutableList<E>) { datas.addAll(data); notifyItemRangeChanged(datas.size-data.size, datas.size) }
+    fun addData(data: MutableList<E>) {
+        datas.addAll(data); notifyItemRangeChanged(datas.size-data.size, datas.size)
+        pag.curPage += 1
+        pag.curTotal = datas.size
+        datasStateCallback?.noMoreData(pag.hasEnd())
+        datasStateCallback?.loadMoreState(true)
+        // datasStateCallback?.loadMoreEnabled(!pag.hasEnd())
+    }
+    fun setDataErr(hasRefresh: Boolean) {
+        datasStateCallback?.apply {
+            if (hasRefresh) {
+                refreshState(false)
+                if (datas.size == 0) noMoreData(true)
+            } else loadMoreState(false)
+        }
+    }
 
     // override fun getItemViewType(position: Int): Int { } // 多布局实现
-    override fun onBindViewHolder(holder: BaseVH<E, ViewBinding>, position: Int) { holder.bind(datas[position]) }
+    override fun onBindViewHolder(holder: BaseVH<E, ViewBinding>, position: Int) { holder.bind(datas, position) }
 
     override fun getItemCount(): Int = datas.size
 
@@ -55,24 +79,32 @@ abstract class BaseAdapter<E> : RecyclerView.Adapter<BaseVH<E, ViewBinding>>() {
     }
 }
 
+open class BaseVH<E, out VB : ViewBinding>(val vb: VB, private val bindViewHolder: BaseVH<*, *>.(vb: VB, entity: E) -> Unit) : RecyclerView.ViewHolder(vb.root) {
+    var entity: E? = null
+    private lateinit var _datas: MutableList<E>
+    fun bind(datas: MutableList<E>, position: Int) { _datas = datas; this.entity = datas[position]; bindViewHolder(this, vb, entity!!) }
+
+    fun isLastPosition() = _datas.size-1 == adapterPosition
+}
+
 abstract class DiffCallback<E> {
     abstract fun areItemsTheSame(od: E, nd: E): Boolean
     fun areContentsTheSame(od: E, nd: E): Boolean = false
     fun getChangePayload(od: E, nd: E): Any? = null
 }
-
-open class BaseVH<E, out VB : ViewBinding>(val vb: VB, private val bindViewHolder: (vb: VB, entity: E) -> Unit) : RecyclerView.ViewHolder(vb.root) {
-    var entity: E? = null
-    fun bind(entity: E) { this.entity = entity; bindViewHolder.invoke(vb, entity) }
+interface DatasStataCallback {
+    fun refreshState(suc: Boolean)          // 刷新成功或失败
+    fun loadMoreState(suc: Boolean)         // 加载更多数据成功或失败
+    fun noMoreData(no: Boolean)             // 没有更多数据
 }
 
 fun ViewGroup.getLayoutInflater(): LayoutInflater { return LayoutInflater.from(context) }
 
 inline fun RecyclerView.removeFocus() { isFocusableInTouchMode = false; requestFocus() }
 inline fun RecyclerView.setLinearLayoutManager(
-                    @RecyclerView.Orientation ort: Int = RecyclerView.VERTICAL,
-                    noScrollV: Boolean = false,
-                    noScrollH: Boolean = false) {
+    @RecyclerView.Orientation ort: Int = RecyclerView.VERTICAL,
+    noScrollV: Boolean = false,
+    noScrollH: Boolean = false) {
     layoutManager = object : LinearLayoutManager2(context, ort, false) {
         override fun canScrollVertically(): Boolean {
             return if (noScrollV) false else super.canScrollVertically()
@@ -83,10 +115,10 @@ inline fun RecyclerView.setLinearLayoutManager(
     }
 }
 inline fun RecyclerView.setGradLayoutManager(
-                    spanCount: Int,
-                    @RecyclerView.Orientation ort: Int = RecyclerView.VERTICAL,
-                    noScrollV: Boolean = false,
-                    noScrollH: Boolean = false) {
+    spanCount: Int,
+    @RecyclerView.Orientation ort: Int = RecyclerView.VERTICAL,
+    noScrollV: Boolean = false,
+    noScrollH: Boolean = false) {
     layoutManager = object : GridLayoutManager2(context, spanCount, ort, false) {
         override fun canScrollVertically(): Boolean {
             return if (noScrollV) false else super.canScrollVertically()
@@ -99,29 +131,22 @@ inline fun RecyclerView.setGradLayoutManager(
 
 /*** 管理分页信息  */
 open class Pagination {
-    constructor() {}
-    constructor(totalSize: Int) { this.totalSize = totalSize }
 
     var curPage = 1
     var curTotal = 0
+
     var pageSize = 20
     var totalPage = -1
     var totalSize = -1
-    fun setTotalPage(totalPage: Int): Pagination {
-        this.totalPage = totalPage; return this
-    }
 
     fun nextPage(r: Boolean): Int = if (r) 1 else curPage + 1
 
-    /**
-     * 是否到达最后一页, (最新返回数据为null时不在考虑范围内, 主要计算data<=pageSize)
-     * @param inPageSize 依据pageSize来计算, 返回数<pageSize></pageSize>
-     *
-     */
-    @JvmOverloads
-    fun hasEnd(inPageSize: Boolean = true): Boolean {
-        return totalPage > -1 && curPage >= totalPage || totalSize > -1 && curTotal >= totalSize || inPageSize && curTotal % pageSize != 0
-    }
+    /*** 是否到达最后一页 ***/
+    fun hasEnd(): Boolean
+            = curTotal > 0 &&
+            ((totalPage > -1 && curPage >= totalPage) ||
+                    (totalSize > -1 && curTotal >= totalSize) ||
+                    ((-1 == totalPage && -1 == totalSize) && curTotal % pageSize != 0))  // 未填充总页数或总数，则以每页约定数量PageSize取模判断是否最后一页
 }
 
 // diffUtil中易出现 Fix Google Bug https://stackoverflow.com/questions/31759171/recyclerview-and-java-lang-indexoutofboundsexception-inconsistency-detected-in/37050829
